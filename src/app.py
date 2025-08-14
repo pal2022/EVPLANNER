108cb87ead915b88fc25723f4bddd12cb2877a63
pal2022 <palkan142000@gmail.com>
2025-08-14 11:53:11 -0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -16,9 +16,7 @@ def test():
 
 
 
-@app.route("/map", methods=["POST"])
-#@app.route("/generate-route", methods=["POST"])
-
+@app.route("/generate-route", methods=["POST"])
 def generate_route():
     print(f"Received POST request to /generate-route")
     print(f"Request headers: {dict(request.headers)}")
